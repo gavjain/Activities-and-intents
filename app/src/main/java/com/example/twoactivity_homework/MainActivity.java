3185e7c232abca95f4161a289e51a887fb70e767
Gaurav <gaurav@Khushis-MacBook-Pro.local>
2022-04-05 22:40:25 +0545

    homework

---
+++ app/src/main/java/com/example/twoactivity_homework/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.twoactivity_homework;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE ="com.example.HelloToast.extra.MESSAGE";
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void launchSecondActivity(View view) {
+        String message = mShowCount.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
