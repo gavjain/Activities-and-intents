bc1be7ba8ab0ffdc8193b57cf855320fd740b45e
Gaurav <gaurav@Khushis-MacBook-Pro.local>
2022-04-05 22:35:28 +0545

    tasks

---
+++ app/src/main/java/com/example/twoactivites/MainActivity.java
@@ -0,0 +1,64 @@
+
+package com.example.twoactivites;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.twoactivites.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int TEXT_REQUEST = 1;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.example.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, com.example.twoactivites.SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        secondActivityResultLauncher.launch(intent);
+    }
+
+
+    ActivityResultLauncher<Intent> secondActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+                String reply = data.getStringExtra(com.example.twoactivites.SecondActivity.EXTRA_REPLY);
+
+                // Make the reply head visible.
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                // Set the reply and make it visible.
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    });
+
+}
